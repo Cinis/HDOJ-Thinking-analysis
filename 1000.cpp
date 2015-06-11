c8f2727130927e1b66399451da474b17ddf1dd3a
And sometimes a dog, I don't know i was <cy1215181830@live.com>
2015-06-12 00:01:03 +0800

    Create 1000.cpp

---
+++ 1000.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum;
+    while(scanf("%d %d",&a,&b)!=EOF)
+    {
+        sum=0;
+        sum=a+b;
+        printf("%d\n",sum);
+    }
+    return 0;
+}
+
+
+
+//the same as poj 1000
