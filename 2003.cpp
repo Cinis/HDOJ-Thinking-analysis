487dd8801fbd961efb32bb3747a793a0d1cdb2c4
And sometimes a dog, I don't know i was <cy1215181830@live.com>
2015-06-18 12:24:59 +0800

    Create 2003.cpp

---
+++ 2003.cpp
@@ -0,0 +1,13 @@
+#include <math.h>
+#include <stdio.h>
+
+int main(void)
+{
+    double r;
+
+    while (scanf("%lf", &r) != EOF)
+        printf("%.2lf\n", fabs(r));
+
+    return 0;
+}
+//use the fabs function
